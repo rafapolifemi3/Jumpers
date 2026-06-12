1263a5247f2e63a5d6379bc51f9f68dd084f6a4d
Rafaele <rafaele_polifemi@hotmail.com>
2026-06-12 10:32:13 -0300

    Primeira versão do projeto

---
+++ code/game.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+
+from code.menu import Menu
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
+
+    def run(self, ):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+            # Check for all events
+            # for event in pygame.event.get():
+            #    if event.type == pygame.QUIT:
+            #       print('Quitting...')
+            #       pygame.quit()  # Close Window
+            #        quit()  # end pygame
---
+++ main.py
@@ -0,0 +1,4 @@
+from code.Game import game
+
+game = Game()
+game.run()
